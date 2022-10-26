a3d1bb967d5f6d019375a0e91e3ed7a8fb9efe3f
TGL <tglokeshbabu@gmail.com>
2022-10-26 11:31:53 +0530

    added procs

--- Innovden/asgi.py
+++ Innovden/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Innovden.Innovden.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Innovden.settings')
 
 application = get_asgi_application()
--- Innovden/wsgi.py
+++ Innovden/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Innovden.Innovden.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Innovden.settings')
 
 application = get_wsgi_application()
