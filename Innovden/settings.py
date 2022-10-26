798efa6803454938de34ea2ec24466e95a7b0a12
SriEagle <86080312+SriEagle@users.noreply.github.com>
2022-10-26 12:40:08 +0530

    Update settings.py

--- Innovden/settings.py
+++ Innovden/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = (os.path.join(os.path.dirname(__file__), '..', 'static'))
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
