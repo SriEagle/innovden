e17c79a6e8e6ce34c45308f135572e38420e64b2
SriEagle <86080312+SriEagle@users.noreply.github.com>
2022-10-26 14:13:00 +0530

    Update settings.py

--- Innovden/Innovden/settings.py
+++ Innovden/Innovden/settings.py
@@ -1,3 +1,4 @@
+import sys
 import os
 from pathlib import Path
 
@@ -110,9 +111,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+if sys.argv[1] != 'runserver':
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
