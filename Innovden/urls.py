4533d7770a94026e06a469cbc9e6901c8757cd4b
SriEagle <86080312+SriEagle@users.noreply.github.com>
2022-10-26 11:51:08 +0530

    Update urls.py

--- Innovden/urls.py
+++ Innovden/urls.py
@@ -10,7 +10,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('/app/admin/', admin.site.urls),
 
     path('base',views.BASE,name='base'),
 
